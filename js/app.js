94e3f6930de90189bc1c92f76985cffb7abe1dd5
raniaaha <rania.hajjout@stud.fra-uas.de>
2022-11-24 10:11:51 +0100

    ..

--- js/app.js
+++ js/app.js
@@ -161,43 +161,43 @@
 //   logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
 // }
 
-// //contacts
-// function readContacts() {
-//   var api = (navigator.contacts || navigator.mozContacts);
+//contacts
+function readContacts() {
+  var api = (navigator.contacts || navigator.mozContacts);
     
-//   if (api && !!api.select) { // new Chrome API
-//     api.select(['name', 'email'], {multiple: true})
-//       .then(function (contacts) {
-//         consoleLog('Found ' + contacts.length + ' contacts.');
-//         if (contacts.length) {
-//           consoleLog('First contact: ' + contacts[0].name + ' (' + contacts[0].email + ')');
-//         }
-//       })
-//       .catch(function (err) {
-//         consoleLog('Fetching contacts failed: ' + err.name);
-//       });
+  if (api && !!api.select) { // new Chrome API
+    api.select(['name', 'email'], {multiple: true})
+      .then(function (contacts) {
+        consoleLog('Found ' + contacts.length + ' contacts.');
+        if (contacts.length) {
+          consoleLog('First contact: ' + contacts[0].name + ' (' + contacts[0].email + ')');
+        }
+      })
+      .catch(function (err) {
+        consoleLog('Fetching contacts failed: ' + err.name);
+      });
       
-//   } else if (api && !!api.find) { // old Firefox OS API
-//     var criteria = {
-//       sortBy: 'familyName',
-//       sortOrder: 'ascending'
-//     };
+  } else if (api && !!api.find) { // old Firefox OS API
+    var criteria = {
+      sortBy: 'familyName',
+      sortOrder: 'ascending'
+    };
 
-//     api.find(criteria)
-//       .then(function (contacts) {
-//         consoleLog('Found ' + contacts.length + ' contacts.');
-//         if (contacts.length) {
-//           consoleLog('First contact: ' + contacts[0].givenName[0] + ' ' + contacts[0].familyName[0]);
-//         }
-//       })
-//       .catch(function (err) {
-//         consoleLog('Fetching contacts failed: ' + err.name);
-//       });
+    api.find(criteria)
+      .then(function (contacts) {
+        consoleLog('Found ' + contacts.length + ' contacts.');
+        if (contacts.length) {
+          consoleLog('First contact: ' + contacts[0].givenName[0] + ' ' + contacts[0].familyName[0]);
+        }
+      })
+      .catch(function (err) {
+        consoleLog('Fetching contacts failed: ' + err.name);
+      });
       
-//   } else {
-//     consoleLog('Contacts API not supported.');
-//   }
-// }
+  } else {
+    consoleLog('Contacts API not supported.');
+  }
+}
 //offline storage
 if ('localStorage' in window || 'sessionStorage' in window) {
   var selectedEngine;
